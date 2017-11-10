ac9cf4b6cc7b7a24a1102af4e5e87e473faec4a9
Michele Mastropietro <michele.mastropietro@ugent.be>
2017-11-10 18:19:53 +0100

    Add content

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Analyze flight from Brussel South Charleroi airport
+
+Functions and script to scrape and analyze the flights servicing CRL.
+
---
+++ scrape_flight_crl/__init__.py
@@ -0,0 +1,48 @@
+import urllib
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+URL = 'https://www.charleroi-airport.com/en/flights/timetable/index.html'
+
+def parse_table(table):
+    col_names = [col.text for col in table.find_all('thead')[0].find_all("th")]
+    flight = table.previous_sibling.previous_sibling.h2.string
+    content = list()
+    for tr in table.find_all('tr'):
+        tds = tr.find_all('td')
+        row = list(td.string for td in tds)
+        if len(row):
+            content.append(row)
+    return flight, col_names, content
+
+def shorten_flight(flight):
+    new_flight = flight[5:]  # It starts always with 'From '
+    new_flight = new_flight.replace("Brussels South Charleroi", "CRL").replace(" to ", "->")
+    return new_flight
+
+def generate_df(tables):
+    dfs = list()
+    for table in tables:
+        flight, col_names, content = parse_table(table)
+        flight = shorten_flight(flight)
+        index = pd.MultiIndex.from_arrays([[flight]*len(content), list(range(len(content)))], names=("connection", "#"))
+        df = pd.DataFrame(content, columns=col_names, index=index)
+        df['Mon.'] = df['Mon.'].astype(bool)
+        df.loc[:, 'Mon.':'Sun.'] = df.loc[:, 'Mon.':'Sun.'].applymap(bool) #.astype(bool) # I have to do this after having explicitly converted (only) one column otherwise it does not work
+        df['Start'] = pd.to_datetime(df['Start'])
+        df['End'] = pd.to_datetime(df['End'])
+        dfs.append(df)
+    return pd.concat(dfs)
+
+def parse_flights(url=URL):
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    tables = soup.find_all('table')
+    return generate_df(tables[:-1])
+
+def get_arrivals(df):
+	return df[df.index.get_level_values('connection').str.endswith('CRL')]
+
+def get_departures(df):
+	return df[df.index.get_level_values('connection').str.startswith('CRL')]
