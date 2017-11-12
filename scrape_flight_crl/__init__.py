5044a9714ad54185c9ede2ce4c6701a35b3c8b05
Michele Mastropietro <michele.mastropietro@ugent.be>
2017-11-12 17:43:17 +0100

    Add possibility to save and load saved pages

--- scrape_flight_crl/__init__.py
+++ scrape_flight_crl/__init__.py
@@ -5,6 +5,10 @@ import pandas as pd
 
 URL = 'https://www.charleroi-airport.com/en/flights/timetable/index.html'
 
+
+def is_url(url):
+    return urllib.parse.urlparse(url).scheme in ('http', 'https',)
+
 def parse_table(table):
     col_names = [col.text for col in table.find_all('thead')[0].find_all("th")]
     flight = table.previous_sibling.previous_sibling.h2.string
@@ -35,14 +39,30 @@ def generate_df(tables):
         dfs.append(df)
     return pd.concat(dfs)
 
-def parse_flights(url=URL):
-    page = urllib.request.urlopen(url)
+def parse_flights(url=URL, save_page=True):
+    if is_url(url):
+        page = urllib.request.urlopen(url)
+    else:
+        page = open(url)
+        save_page = False
     soup = BeautifulSoup(page, 'html.parser')
+    if save_page:
+        dump_page(url)
     tables = soup.find_all('table')
     return generate_df(tables[:-1])
 
+def dump_page(url):
+    from datetime import datetime
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    filename = "backup/url_{}.html".format(datetime.now().isoformat(timespec='hours'))
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(str(soup))
+        f.close()
+    return filename
+
 def get_arrivals(df):
-	return df[df.index.get_level_values('connection').str.endswith('CRL')]
+    return df[df.index.get_level_values('connection').str.endswith('CRL')]
 
 def get_departures(df):
-	return df[df.index.get_level_values('connection').str.startswith('CRL')]
+    return df[df.index.get_level_values('connection').str.startswith('CRL')]
